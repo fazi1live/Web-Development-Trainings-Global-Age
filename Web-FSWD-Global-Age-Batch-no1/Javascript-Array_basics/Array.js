c124d96bce1f0003d519d50bd750d38b16206bb0
fazi1live <faziislive@gmail.com>
2022-12-18 20:30:06 +0500

    add

---
+++ Web-FSWD-Global-Age-Batch-no1/Javascript-Array_basics/Array.js
@@ -0,0 +1,32 @@
+
+// let Numbers = [10,11,12,13,14,15,16,17,18] 
+// console.log(Numbers);
+// console.log(typeof(Numbers))
+
+// let Names = ['Ali','Ahmed','Abdul','Jamil']
+// console.log(typeof(Names))
+
+// let Humans = [
+//     {
+//         Name:'Ali',
+//         Age:8
+//     },
+//     {
+//         Name:'Ahmed',
+//         Age:12
+//     },
+//     {
+//         Name:'Abdul',
+//         Age:20
+//     }
+// ]
+
+// Humans.push('Hello World')
+// console.log(Humans)
+// let lastvalue = Humans.pop();
+// console.log(lastvalue)
+
+
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const citrus = fruits.slice(2,3);
+console.log(citrus)
--- Web-FSWD-Global-Age-Batch-no1/Travel-Agency-Online/js/dashboard.js
+++ Web-FSWD-Global-Age-Batch-no1/Travel-Agency-Online/js/dashboard.js
@@ -5,12 +5,14 @@ let createCard = (rawData) => {
         const heading_1 = formValue.get('heading-1');
         const heading_2 = formValue.get('heading-2');
         const description = formValue.get('description');
+        const productArray = [];
         const productObject = {};
         productObject.imageUrl = imageUrl;
         productObject.heading_1 = heading_1;
         productObject.heading_2 = heading_2;
         productObject.description = description;
-        localStorage.setItem('product-data',JSON.stringify(productObject))
+        productArray.push(productObject);
+        localStorage.setItem('product-data',JSON.stringify(productArray))
     } catch (error) {
         console.log(error)
     }
